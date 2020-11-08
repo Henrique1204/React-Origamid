22d5bf2094a162abff3e57ceaa375a5aacfb0a8e
Paulo <pauloh16.pdr@gmail.com>
2020-11-08 18:41:14 -0300

    Adicionar correcao do exercicio JSX-Array

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -2,39 +2,46 @@
 // Mostre apenas produtos que forem mais caros que R$ 1500
 const produtos = [
     {
-      id: 1,
-      nome: 'Smartphone',
-      preco: 'R$ 2000',
-      cores: ['#29d8d5', '#252a34', '#fc3766'],
+        id: 1,
+        nome: 'Smartphone',
+        preco: 'R$ 2000',
+        cores: ['#29d8d5', '#252a34', '#fc3766'],
     },
     {
-      id: 2,
-      nome: 'Notebook',
-      preco: 'R$ 3000',
-      cores: ['#ffd045', '#d4394b', '#f37c59'],
+        id: 2,
+        nome: 'Notebook',
+        preco: 'R$ 3000',
+        cores: ['#ffd045', '#d4394b', '#f37c59'],
     },
     {
-      id: 3,
-      nome: 'Tablet',
-      preco: 'R$ 1500',
-      cores: ['#365069', '#47c1c8', '#f95786'],
+        id: 3,
+        nome: 'Tablet',
+        preco: 'R$ 1500',
+        cores: ['#365069', '#47c1c8', '#f95786'],
     },
-  ];
-  
-  const App = () => {
+];
+
+const App = () => {
+    const dados = produtos.filter(({preco}) => Number(preco.replace("R$ ", "")) > 1500);
+    
     return (
         <section>
             <ul style={{listStyle: "none", padding: "0px"}}>
                 {
-                    produtos.filter((p) => Number(p["preco"].replace("R$ ", "")) > 1500)
-                    .map(({id, nome, preco, cores}) => (
+                    dados.map(({id, nome, preco, cores}) => (
                         <li key={id}>
                             <h2>{nome}</h2>
                             <p>Preço: {preco}</p>
                             <ul style={{listStyle: "none"}}>
                                 {
                                     cores.map(c => (
-                                        <li key={c} style={{color: "#FFF", backgroundColor: c, padding: "5px 10px", textTransform: "uppercase"}}>
+                                        <li key={c}
+                                            style={{color: "#FFF",
+                                                    backgroundColor: c,
+                                                    padding: "5px 10px",
+                                                    textTransform: "uppercase"
+                                                }}
+                                        >
                                             {c}
                                         </li>
                                     ))
@@ -46,7 +53,8 @@ const produtos = [
             </ul>
         </section>
     );
-  };
-  
-
+};
+            
+            
 export default App;
+            
