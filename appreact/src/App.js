524fc6bbdfe67aedf35dbd631f5a2f4634ce9eb0
Paulo <pauloh16.pdr@gmail.com>
2020-11-15 23:42:25 -0300

    Adicionar exemplo sobre link

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -4,6 +4,7 @@ import Header from "./Header.js";
 import Home from "./Home.js";
 import Sobre from "./Sobre.js";
 import Footer from "./Footer.js";
+import Login from './Login.js';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="sobre" element={<Sobre />} />
+        <Route path="login" element={<Login />} />
         <Route path="*" element={<h1>404</h1>} />
       </Routes>
   
--- appreact/src/Header.js
+++ appreact/src/Header.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
     return (
         <header>
             <h1>Header</h1>
+
+            <nav style={{display: "flex", justifyContent: "space-between", maxWidth: "400px"}}>
+                <NavLink to="/" end activeStyle={{color: "tomato"}} >Home</NavLink>
+                <NavLink to="sobre" activeStyle={{color: "tomato"}} >Sobre</NavLink>
+                <NavLink to="login" activeStyle={{color: "tomato"}} >Login</NavLink>
+            </nav>
         </header>
     );
 };
